2c0ce6fa4947b05ba578611de14cdd8ff667acf8
Paola931 <145440087+Paola931@users.noreply.github.com>
2023-10-30 14:36:09 +0100

    Update Exception1.java
    correzione

--- Exception1/Exception1.java
+++ Exception1/Exception1.java
@@ -1,7 +1,5 @@
-package MyExercisesJavaAdvanced.Exception1;
-
 public class Exception1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         int num = 5;
         int min = 5;
         int max = 15;
@@ -10,20 +8,19 @@ public class Exception1 {
         System.out.println(numInRange(num,min,max));
         testError(num, min, max);
     }
-    public static boolean numInRange(int num, int min, int max) {
+    public static boolean numInRange(int num, int min, int max) throws Exception {
         if (num >= min && num <= max) {
             return true;
         } else {
-            return false;
+            throw new Exception( num +"si trova fuori dal range");
         }
 
     }
-    public static void testError (int num, int min , int max) {
+    public static void testError (int num, int min , int max) throws Exception {
         boolean inRange = numInRange(num, min, max);
         try {
-            if (!inRange) {
-                throw new Exception( num +"si trova fuori dal range");
-            }
+          if(inRange){
+          }
         } catch (Exception e) {
             System.out.println("Errore " + e);
         }
