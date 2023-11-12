bf78be04ac5a35b023e1d5c2e4b500311bbded64
Paola931 <145440087+Paola931@users.noreply.github.com>
2023-11-12 15:27:57 +0100

    Update Exception2.java
    Correzioni

--- Exception2/Exception2.java
+++ Exception2/Exception2.java
@@ -1,17 +1,22 @@
 public class Exception2 {
-    public static void main(String[]args) throws Exception {
-        char x = 'h';
-        checkChar(isChar(x),x);
+    public static void main(String[]args) {
+        char x = '5';
+
+        try{
+            checkChar(isChar(x),x);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
     }
     public static boolean isChar (char x) throws Exception {
         if(Character.isDigit(x)) {
-            throw new Exception ("This is a number, please choose a character");
+            throw new Exception("This is a number, please choose a character");
         }else{
             return true;
         }
     }
-    public static String checkChar(boolean isChar,char x){
+    public static String checkChar(boolean isChar, char x){
         try{
             if(isChar){
                 System.out.print("You have chosen this character: " + x);
