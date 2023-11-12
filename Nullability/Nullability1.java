1b0f089518e8769f684da4990b48ade8f1beba79
Paola931 <145440087+Paola931@users.noreply.github.com>
2023-11-12 15:30:50 +0100

    Update Nullability1.java
    Correzioni

--- Nullability/Nullability1.java
+++ Nullability/Nullability1.java
@@ -1,29 +1,26 @@
-package MyExercisesJavaAdvanced.Nullability;
+package Nullability;
 
-public class Nullability1 {
+public class Nullability {
     public static void main (String[]args) throws Exception {
         Integer num = 20;
         Integer den = 30;
 
-        System.out.println(nullCheck(num,den));
-        tryCheck(num,den);
+        try{
+            System.out.println(nullCheck(num,den));
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
-    public static boolean nullCheck (Integer num,Integer den) throws Exception {
-        if(num == null || den == null){
-            throw new Exception("Un valore corrisponde a null, impostare nuovo valore");
+    public static boolean nullCheck (Integer num,Integer den) {
+        if(num != null && den != null){
+            System.out.println("Il numeratore corrisponde a: \"" + num + "\" e il denominatore corrisponde a: \""+ den + "\"");
+        }else{
+            throw new NullPointerException("Il numeratore o il denominatore risulta NULL");
         }
         return true;
     }
-    public static String tryCheck (Integer num,Integer den){
-        try{
-            if(num != null && den != null){
-                System.out.println("Il numeratore corrisponde a: \"" + num + "\" e il denominatore corrisponde a: \""+ den + "\"");
-            }
-        } catch (Exception e) {
-            System.out.println("Errore: " +e);
-        }
-        return null;
-    }
+
 }
 
     /*Scrivere una funzione che accetti in input un numeratore e un denominatore
