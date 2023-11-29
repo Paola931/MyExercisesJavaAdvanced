5a88d81b85038b5dc4bfa465999a8385a4786ef2
Paola931 <145440087+Paola931@users.noreply.github.com>
2023-11-29 22:06:00 +0100

    Update Main.java
    correzione

--- OperatoreTernario/OperatoreTernario2/Main.java
+++ OperatoreTernario/OperatoreTernario2/Main.java
@@ -4,21 +4,10 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             System.out.println("Valore: " + i);
-            //System.out.println(checkValue(i));
             System.out.println(checkOP(i));
         }
     }
 
-    private static String checkValue(int value) {
-        String result = "";
-
-        if (value % 5 == 0) {
-            result = "FizzBuzz";
-        } else  {
-            result = "Buzz";
-        }
-        return result;
-    }
     public static String checkOP(int value){
         String result = value % 5 == 0 ? "FizzBuzz" : "Buzz";
 
