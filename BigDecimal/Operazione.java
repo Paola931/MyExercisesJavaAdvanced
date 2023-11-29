1a38462169d2d9c98fff8088b6ed4cf550449748
Paola931 <145440087+Paola931@users.noreply.github.com>
2023-11-29 22:02:48 +0100

    Update Operazione.java
    correzione

--- BigDecimal/Operazione.java
+++ BigDecimal/Operazione.java
@@ -13,20 +13,22 @@ public class Operazione {
         MIN,
         MAX
     }
-    BigDecimal num1;
-    BigDecimal num2;
+    private BigDecimal num1;
+    private BigDecimal num2;
     private final Tipo tipo;
-    public Operazione(Tipo tipo, BigDecimal num1 , BigDecimal num2) {
-        this.num1=num1;
-        this.num2=num2;
-        this.tipo=tipo;
+
+
+    public Operazione(BigDecimal num1, BigDecimal num2, Tipo tipo) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.tipo = tipo;
     }
 
     public static String operazioni(Tipo tipo, BigDecimal num1 , BigDecimal num2){
         if(num1 != null && num2 != null) {
             switch (tipo) {
                 case ADDIZIONE:
-                    add(num1, num2);
+                    add(num1,num2);
                     break;
                 case SOTTRAZIONE:
                     sub(num1, num2);
@@ -57,34 +59,40 @@ public class Operazione {
         return tipo;
     }
 
-    public static void add (BigDecimal num1 , BigDecimal num2){
+    public static BigDecimal add(BigDecimal num1, BigDecimal num2){
         System.out.println("Hai scelto l'operazione " + Tipo.ADDIZIONE + " tra il " + num1 + " e il " + num2);
         BigDecimal result = num1.add(num2);
         System.out.println("Il risultato è: " + result);
+        return result;
     }
-    public static void sub (BigDecimal num1 , BigDecimal num2){
+    public static BigDecimal sub (BigDecimal num1 , BigDecimal num2){
         System.out.println("Hai scelto l'operazione " + Tipo.SOTTRAZIONE + " tra il " + num1 + " e il " + num2);
         BigDecimal result = num1.subtract(num2);
         System.out.println("Il risultato è: " + result);
+        return result;
     }
-    public static void  mul (BigDecimal num1 , BigDecimal num2){
+    public static BigDecimal  mul (BigDecimal num1 , BigDecimal num2){
         System.out.println("Hai scelto l'operazione " + Tipo.MOLTIPLICAZIONE + " tra il " + num1 + " e il " + num2);
         BigDecimal result = num1.multiply(num2);
         System.out.println("Il risultato è: " + result);
+        return result;
     }
-    public static void div (BigDecimal num1 , BigDecimal num2){
+    public static BigDecimal div (BigDecimal num1 , BigDecimal num2){
         System.out.println("Hai scelto l'operazione " + Tipo.DIVISIONE + " tra il " + num1 + " e il " + num2);
         BigDecimal result = num1.divide(num2, RoundingMode.HALF_EVEN);
         System.out.println("Il risultato è: " + result);
+        return result;
     }
-    public static void min (BigDecimal num1 , BigDecimal num2){
+    public static BigDecimal min (BigDecimal num1 , BigDecimal num2){
         System.out.println("Hai scelto l'operazione " + Tipo.MIN + " tra il " + num1 + " e il " + num2);
         BigDecimal result = num1.min(num2);
         System.out.println("Il numero minore è: " + result);
+        return result;
     }
-    public static void max (BigDecimal num1 , BigDecimal num2){
+    public static BigDecimal max (BigDecimal num1 , BigDecimal num2){
         System.out.println("Hai scelto l'operazione " + Tipo.MAX + " tra il " + num1 + " e il " + num2);
         BigDecimal result = num1.max(num2);
         System.out.println("Il numero maggiore è: " + result);
+        return result;
     }
 }
